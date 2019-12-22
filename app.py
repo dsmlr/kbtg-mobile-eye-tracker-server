49eef6dc0882303d21bb464c409af89c40dd6d35
Mawin Jongkrirattanakul <mw.jkrtnk@gmail.com>
2019-12-23 03:06:18 +0700

    add endpoint for recieve video from android demo

--- app.py
+++ app.py
@@ -10,16 +10,26 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/calibrate', methods=["POST"])
 def calibrate():
-    print(request)
     if 'image[]' not in request.files:
         return {'message': 'No image in the request'}, 400
 
     images = request.files.getlist("image[]")
 
-    print(images)
-
     for image in images:
         image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename + ".jpeg"))
+
+    return {'message': 'Upload Successfully'}, 200
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    if 'video[]' not in request.files:
+        return {'message': 'No video in the request'}, 400
+
+    videos = request.files.getlist("video[]")
+
+    for video in videos:
+        video.save(os.path.join(app.config['UPLOAD_FOLDER'], video.filename))
 
     return {'message': 'Upload Successfully'}, 200
 
